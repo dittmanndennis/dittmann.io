632c243f13739fe1444d1c39e56fbcff8d6e6a3b
dittmanndennis <dennis1dittmann@gmail.com>
2022-02-19 13:37:47 +0100

    newest design

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = createTheme({
+  palette: {
+    LinkedIn: {
+      main: '#0e76a8',
+    },
+    GitHub: {
+      main: '#171515',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <Stack spacing={8} direction="column" alignItems="center">
+            <Stack spacing={2} direction="row">
+              <Button variant="contained" color="info" href="mailto: dennis@dittmann.io"><EmailIcon/></Button>
+              <p>dennis@dittmann.io</p>
+            </Stack>
 
-        <div className="ButtonGroup">
-          <a
-            className="E-Mail"
-            href="https://www.linkedin.com/in/dennisdittmann/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="Button">LinkedIn</button>
-          </a>
-          <a
-            className="E-Mail"
-            href="https://github.com/dittmanndennis"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="Button">GitHub</button>
-          </a>
-        </div>
-        <a
-          className="E-Mail"
-          href="mailto: dennis@dittmann.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          dennis@dittmann.io
-        </a>
-      </header>
-    </div>
+            <Stack spacing={4} direction="row">
+              <Button variant="contained" color="LinkedIn" size="large" href="https://www.linkedin.com/in/dennisdittmann/"><LinkedInIcon/></Button>
+              <Button variant="contained" color="GitHub" size="large" href="https://github.com/dittmanndennis"><GitHubIcon/></Button>
+            </Stack>
+          </Stack>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
